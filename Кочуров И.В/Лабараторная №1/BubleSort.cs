e597978b9cba97f4ed2e7cdadc6b7c1d9943cf91
SaintPlut <SaintPlut@users.noreply.github.com>
2021-02-02 19:28:47 +0400

    upgrade

---
+++ Кочуров И.В/Лабараторная №1/BubleSort.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace BubleSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            // Исходный массив 
+            int[] nums = { 1,9,2,4,7,5,6,8};
+            Console.WriteLine("Массив до сортировки");
+            for (int k = 0; k < nums.Length; k++)
+            {
+                Console.Write(nums[k] + " ");
+            }
+            Console.WriteLine();
+            // Сортировка массива
+             for(int i=1; i<nums.Length; i++)
+            {
+                for(int j=0; j<nums.Length-i; j++)
+                {
+                    if(nums[j]>nums[j+1])
+                    {
+                        a = nums[j + 1];
+                        nums[j + 1] = nums[j];
+                        nums[j] = a;
+                    }
+                }
+            }
+            Console.WriteLine("Массив после сортировки");
+            for(int k=0; k<nums.Length; k++)
+            {
+                Console.Write(nums[k] + " ");
+            }
+        }
+    }
+}
+   
+      
---
+++ Кочуров И.В/Лабараторная №2/Стек.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace ArrStack
+{
+    class Program
+    {
+        static void Push(Stack<int> stack, int numbers, int Count)
+        {
+            stack.Push(numbers);
+        }
+        static void Pop(Stack<int> stack)
+        {
+            if (stack.Count == 0)
+                throw new NullReferenceException("Пустой стек");
+        }
+        static Stack<int> CloneStack(Stack<int> stack)
+        {
+            Stack<int> cloneNumbers = new Stack<int>(stack);
+            return cloneNumbers;
+        }
+        static void MemCleaner()
+        {
+            Console.WriteLine(GC.GetTotalMemory(false));
+            GC.Collect();
+            Console.WriteLine(GC.GetTotalMemory(true));
+        }
+
+        static void PrintStack(Stack<int> stack)
+        {
+            foreach (int bit in stack)
+            {
+                Console.Write($"{bit}  ");
+            }
+            Console.WriteLine();
+        }
+        static void Main(string[] args)
+        {
+            Stack<int> numbers = new Stack<int>();
+            Console.WriteLine("Заполняем стек");
+            numbers.Push(1);
+            numbers.Push(4);
+            numbers.Push(3);
+            numbers.Push(2);
+            numbers.Push(5);
+            Console.WriteLine("Результат");
+            PrintStack(numbers);
+            Console.WriteLine("Удаляем верхний элемент");
+            numbers.Pop();
+            PrintStack(numbers);
+            Console.WriteLine();
+            Console.WriteLine("Копируем содержимое в массив");
+            int[] Array = numbers.ToArray();
+            foreach (int i in Array)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine("Результат очищения памяти");
+            MemCleaner();
+        }
+    }
+}
